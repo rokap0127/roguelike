585fe15feec72424519f8ac2fb1a97bb2c7ac87d
rokap0127 <r.okap0127@gmail.com>
2020-01-14 04:35:00 +0900

    MiniSlime当たり判定 Hp 追加

--- Assets/Script/Boss/MiniSlime.cs
+++ Assets/Script/Boss/MiniSlime.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class MiniSlime : MonoBehaviour
 {
-    public float moveSpeed; 
+    public float moveSpeed;
+   
+    public int enemyMaxHp; //最大Hp
+    public int enemyHp; //現在のHp
+
+    public Explotion explosionPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyHp = enemyMaxHp; //最大Hpにする
     }
 
     // Update is called once per frame
@@ -28,5 +33,24 @@ public class MiniSlime : MonoBehaviour
 
         //プレイヤーが存在する方向へ移動する
         transform.localPosition += direction * moveSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "PlayerAttack")
+        {
+            //エフェクト生成
+            Instantiate(
+    explosionPrefab,
+    collision.transform.position,
+    Quaternion.identity);
+            //敵のHPを減らす
+            enemyHp--;
+            //敵のHPがまだ残っている場合はここで処理を終える
+            if (0 < enemyHp) { return; }
+
+            //敵を削除する
+            Destroy(gameObject);
+        }
     }
 }
