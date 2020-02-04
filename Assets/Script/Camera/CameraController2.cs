5325e86ae9ffc5752111331549e068190e663bdb
fujishu <shu2011332@docomo.ne.jp>
2020-02-04 09:42:07 +0900

    ステージ1を非ランダム化

--- Assets/Script/Camera/CameraController2.cs
+++ Assets/Script/Camera/CameraController2.cs
@@ -7,23 +7,6 @@ public class CameraController2 : MonoBehaviour
 {
     public GameObject[] sceneCamera;
     public GameObject[] entrance;
-    public GameObject[] enemy;
-    int enemynum;
-    public int enemylim;
-    bool callEnemy1;
-    bool callEnemy2;
-    bool callEnemy3;
-    bool callEnemy4;
-    bool callEnemy5;
-    bool callEnemy6;
-    public GameObject[] item;
-    public GameObject[] rareItem;
-    public int itemlim;
-    int itemnum;
-    int rareItemnum;
-    public GameObject[] gimmick;
-    int gimmicknum;
-    public int gimmicklim;
 
     GameObject operation;
     Operation op;
@@ -76,46 +59,6 @@ public class CameraController2 : MonoBehaviour
             sceneCamera[5].SetActive(false);
             sceneCamera[6].SetActive(false);
             sceneCamera[7].SetActive(false);
-            if (!callEnemy1)
-            {
-                int i = 0;
-                itemnum = Random.Range(0, 3);
-                itemlim = Random.Range(3, 8);
-                while (i < itemlim)
-                {
-                    if (itemnum >= 2)
-                    {
-                        rareItemnum = Random.Range(0, 5);
-                        Instantiate(rareItem[rareItemnum], new Vector3(Random.Range(-4.0f, 1.1f), Random.Range(-4.0f, -1.5f), 0), transform.rotation);
-                    }
-                    else
-                    {
-                        Instantiate(item[itemnum], new Vector3(Random.Range(-4.0f, 1.1f), Random.Range(-4.0f, -1.5f), 0), transform.rotation);
-                    }
-                    itemnum = Random.Range(0, 3);
-                    i++;
-                }
-                int g = 0;
-                gimmicknum = Random.Range(0, 2);
-                gimmicklim = Random.Range(0, 3);
-                while (g < gimmicklim)
-                {
-                    Instantiate(gimmick[gimmicknum], new Vector3(Random.Range(-4.0f, 1.1f), Random.Range(-4.0f, -1.5f), 0), transform.rotation);
-                    gimmicknum = Random.Range(0, 2);
-                    g++;
-                }
-                callEnemy1 = true;
-                int e = 0;
-                enemynum = Random.Range(0, 2);
-                enemylim = Random.Range(2, 4);
-                while (e < enemylim)
-                {
-                    Instantiate(enemy[enemynum], new Vector3(Random.Range(-4.0f, 1.1f), Random.Range(-4.0f, -1.5f), 0), transform.rotation);
-                    enemynum = Random.Range(0, 2);
-                    e++;
-                }
-                callEnemy1 = true;
-            }
         }
         if (pos.y > entrance[1].transform.position.y && pos.x > entrance[0].transform.position.x&&pos.x< entrance[2].transform.position.x)
         {
@@ -126,45 +69,6 @@ public class CameraController2 : MonoBehaviour
             sceneCamera[5].SetActive(false);
             sceneCamera[6].SetActive(false);
             sceneCamera[7].SetActive(false);
-            if (!callEnemy2)
-            {
-                int i = 0;
-                itemnum = Random.Range(0, 3);
-                itemlim = Random.Range(3, 8);
-                while (i < itemlim)
-                {
-                    if (itemnum >= 2)
-                    {
-                        rareItemnum = Random.Range(0, 5);
-                        Instantiate(rareItem[rareItemnum], new Vector3(Random.Range(-4.0f, 1.1f), Random.Range(0.1f, 2.3f), 0), transform.rotation);
-                    }
-                    else
-                    {
-                        Instantiate(item[itemnum], new Vector3(Random.Range(-4.0f, 1.1f), Random.Range(0.1f, 2.3f), 0), transform.rotation);
-                    }
-                    itemnum = Random.Range(0, 3);
-                    i++;
-                }
-                int g = 0;
-                gimmicknum = Random.Range(0, 2);
-                gimmicklim = Random.Range(0, 3);
-                while (g < gimmicklim)
-                {
-                    Instantiate(gimmick[gimmicknum], new Vector3(Random.Range(-4.0f, 1.1f), Random.Range(0.1f, 2.3f), 0), transform.rotation);
-                    gimmicknum = Random.Range(0, 2);
-                    g++;
-                }
-                int e = 0;
-                enemynum = Random.Range(0, 2);
-                enemylim = Random.Range(2, 4);
-                while (e < enemylim)
-                {
-                    Instantiate(enemy[enemynum], new Vector3(Random.Range(-4.0f, 1.1f), Random.Range(0.1f, 2.3f), 0), transform.rotation);
-                    enemynum = Random.Range(0, 2);
-                    e++;
-                }
-                callEnemy2 = true;
-            }
         }
         if (pos.x > entrance[2].transform.position.x)
         {
@@ -175,45 +79,6 @@ public class CameraController2 : MonoBehaviour
             sceneCamera[5].SetActive(false);
             sceneCamera[6].SetActive(false);
             sceneCamera[7].SetActive(false);
-            if (!callEnemy3)
-            {
-                int i = 0;
-                itemnum = Random.Range(0, 3);
-                itemlim = Random.Range(3, 8);
-                while (i < itemlim)
-                {
-                    if (itemnum >= 2)
-                    {
-                        rareItemnum = Random.Range(0, 5);
-                        Instantiate(rareItem[rareItemnum], new Vector3(Random.Range(3.3f, 10.4f), Random.Range(-4.0f, -0.5f), 0), transform.rotation);
-                    }
-                    else
-                    {
-                        Instantiate(item[itemnum], new Vector3(Random.Range(3.3f, 10.4f), Random.Range(-4.0f, -0.5f), 0), transform.rotation);
-                    }
-                    itemnum = Random.Range(0, 3);
-                    i++;
-                }
-                int g = 0;
-                gimmicknum = Random.Range(0, 2);
-                gimmicklim = Random.Range(0, 3);
-                while (g < gimmicklim)
-                {
-                    Instantiate(gimmick[gimmicknum], new Vector3(Random.Range(3.3f, 10.4f), Random.Range(-4.0f, -0.5f), 0), transform.rotation);
-                    gimmicknum = Random.Range(0, 2);
-                    g++;
-                }
-                int e = 0;
-                enemynum = Random.Range(0, 2);
-                enemylim = Random.Range(2, 4);
-                while (e < enemylim)
-                {
-                    Instantiate(enemy[enemynum], new Vector3(Random.Range(3.3f, 10.4f), Random.Range(-4.0f, -0.5f), 0), transform.rotation);
-                    enemynum = Random.Range(0, 2);
-                    e++;
-                }
-                callEnemy3 = true;
-            }
         }
         if (pos.x > entrance[2].transform.position.x && pos.y > entrance[3].transform.position.y)
         {
@@ -224,45 +89,6 @@ public class CameraController2 : MonoBehaviour
             sceneCamera[5].SetActive(true);
             sceneCamera[6].SetActive(false);
             sceneCamera[7].SetActive(false);
-            if (!callEnemy4)
-            {
-                int i = 0;
-                itemnum = Random.Range(0, 3);
-                itemlim = Random.Range(3, 8);
-                while (i < itemlim)
-                {
-                    if (itemnum >= 2)
-                    {
-                        rareItemnum = Random.Range(0, 5);
-                        Instantiate(rareItem[rareItemnum], new Vector3(Random.Range(4.3f, 9.4f), Random.Range(1.1f, 3.6f), 0), transform.rotation);
-                    }
-                    else
-                    {
-                        Instantiate(item[itemnum], new Vector3(Random.Range(4.3f, 9.4f), Random.Range(1.1f, 3.6f), 0), transform.rotation);
-                    }
-                    itemnum = Random.Range(0, 3);
-                    i++;
-                }
-                int g = 0;
-                gimmicknum = Random.Range(0, 2);
-                gimmicklim = Random.Range(0, 3);
-                while (g < gimmicklim)
-                {
-                    Instantiate(gimmick[gimmicknum], new Vector3(Random.Range(4.3f, 9.4f), Random.Range(1.1f, 3.6f), 0), transform.rotation);
-                    gimmicknum = Random.Range(0, 2);
-                    g++;
-                }
-                int e = 0;
-                enemynum = Random.Range(0, 2);
-                enemylim = Random.Range(2, 4);
-                while (e < enemylim)
-                {
-                    Instantiate(enemy[enemynum], new Vector3(Random.Range(4.3f, 9.4f), Random.Range(1.1f, 3.6f), 0), transform.rotation);
-                    enemynum = Random.Range(0, 2);
-                    e++;
-                }
-                callEnemy4 = true;
-            }
         }
         if (pos.y > entrance[1].transform.position.y && pos.x < entrance[0].transform.position.x)
         {
@@ -273,45 +99,6 @@ public class CameraController2 : MonoBehaviour
             sceneCamera[5].SetActive(false);
             sceneCamera[6].SetActive(true);
             sceneCamera[7].SetActive(false);
-            if (!callEnemy5)
-            {
-                int i = 0;
-                itemnum = Random.Range(0, 3);
-                itemlim = Random.Range(3, 8);
-                while (i < itemlim)
-                {
-                    if (itemnum >= 2)
-                    {
-                        rareItemnum = Random.Range(0, 5);
-                        Instantiate(rareItem[rareItemnum], new Vector3(Random.Range(-11.3f, -6.2f), Random.Range(-0.1f, 2.3f), 0), transform.rotation);
-                    }
-                    else
-                    {
-                        Instantiate(item[itemnum], new Vector3(Random.Range(-11.3f, -6.2f), Random.Range(-0.1f, 2.3f), 0), transform.rotation);
-                    }
-                    itemnum = Random.Range(0, 3);
-                    i++;
-                }
-                int g = 0;
-                gimmicknum = Random.Range(0, 2);
-                gimmicklim = Random.Range(0, 3);
-                while (g < gimmicklim)
-                {
-                    Instantiate(gimmick[gimmicknum], new Vector3(Random.Range(-11.3f, -6.2f), Random.Range(-0.1f, 2.3f), 0), transform.rotation);
-                    gimmicknum = Random.Range(0, 2);
-                    g++;
-                }
-                int e = 0;
-                enemynum = Random.Range(0, 2);
-                enemylim = Random.Range(2, 4);
-                while (e < enemylim)
-                {
-                    Instantiate(enemy[enemynum], new Vector3(Random.Range(-11.3f, -6.2f), Random.Range(-0.1f, 2.3f), 0), transform.rotation);
-                    enemynum = Random.Range(0, 2);
-                    e++;
-                }
-                callEnemy5 = true;
-            }
         }
         if (pos.y > entrance[4].transform.position.y && pos.x < entrance[1].transform.position.x)
         {
@@ -322,45 +109,6 @@ public class CameraController2 : MonoBehaviour
             sceneCamera[5].SetActive(false);
             sceneCamera[6].SetActive(false);
             sceneCamera[7].SetActive(true);
-            if (!callEnemy6)
-            {
-                int i = 0;
-                itemnum = Random.Range(0, 3);
-                itemlim = Random.Range(3, 8);
-                while (i < itemlim)
-                {
-                    if (itemnum >= 2)
-                    {
-                        rareItemnum = Random.Range(0, 5);
-                        Instantiate(rareItem[rareItemnum], new Vector3(Random.Range(-12.3f, -5.25f), Random.Range(3.8f, 7.15f), 0), transform.rotation);
-                    }
-                    else
-                    {
-                        Instantiate(item[itemnum], new Vector3(Random.Range(-12.3f, -5.25f), Random.Range(3.8f, 7.15f), 0), transform.rotation);
-                    }
-                    itemnum = Random.Range(0, 3);
-                    i++;
-                }
-                int g = 0;
-                gimmicknum = Random.Range(0, 2);
-                gimmicklim = Random.Range(0, 3);
-                while (g < gimmicklim)
-                {
-                    Instantiate(gimmick[gimmicknum], new Vector3(Random.Range(-12.3f, -5.25f), Random.Range(3.8f, 7.15f), 0), transform.rotation);
-                    gimmicknum = Random.Range(0, 2);
-                    g++;
-                }
-                int e = 0;
-                enemynum = Random.Range(0, 2);
-                enemylim = Random.Range(3, 5);
-                while (e < enemylim)
-                {
-                    Instantiate(enemy[enemynum], new Vector3(Random.Range(-12.3f, -5.25f), Random.Range(3.8f, 7.15f), 0), transform.rotation);
-                    enemynum = Random.Range(0, 2);
-                    e++;
-                }
-                callEnemy6 = true;
-            }
         }
         //if (Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl))
         //{
--- Assets/Script/Item/Key.cs
+++ Assets/Script/Item/Key.cs
@@ -5,26 +5,35 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     public GameObject[] shutter;
-    public GameObject bridge;
+    public GameObject[] bridge;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Knight")
         {
-            bridge.SetActive(true);
+            bridge[0].SetActive(true);
+            bridge[1].SetActive(true);
             shutter[0].SetActive(false);
             shutter[1].SetActive(false);
+            shutter[2].SetActive(false);
+            shutter[3].SetActive(false);
         }
         if (collision.gameObject.tag == "Archer")
         {
-            bridge.SetActive(true);
+            bridge[0].SetActive(true);
+            bridge[1].SetActive(true);
             shutter[0].SetActive(false);
             shutter[1].SetActive(false);
+            shutter[2].SetActive(false);
+            shutter[3].SetActive(false);
         }
         if (collision.gameObject.tag == "Mage")
         {
-            bridge.SetActive(true);
+            bridge[0].SetActive(true);
+            bridge[1].SetActive(true);
             shutter[0].SetActive(false);
             shutter[1].SetActive(false);
+            shutter[2].SetActive(false);
+            shutter[3].SetActive(false);
         }
     }
 }
