aaf1c30e017775a55d8c6ae7ae5579775bf0299a
Craig890 <moralescraig33@yahoo.com>
2020-02-18 12:59:43 +0900

    Textを追加

---
+++ Assets/TextHUD.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextHUD : MonoBehaviour
+{
+    public Text TextHud;
+
+    public GameObject Key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision == Key)
+        {
+            TextHud.text = "鍵を取りました！";
+        }
+    }
+}
