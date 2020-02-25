91c3ea6cdfdbc285c0539abedbf572c06eb508a2
rokap0127 <r.okap0127@gmail.com>
2020-02-25 13:46:33 +0900

    ダメージUIを追加

---
+++ Assets/DamageUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageUI : MonoBehaviour
+{
+    private float fadeOutSpeed = 1.0f;
+    private float moveSpeed = 0.4f;
+    Text damageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+        damageText.color = Color.Lerp(damageText.color, new Color(1.0f, 0f, 0f, 0f), fadeOutSpeed * Time.deltaTime);
+
+        if (damageText.color.a <= 0.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
